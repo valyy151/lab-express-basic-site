8e5293580ce55904db46574b336cba2461399cb4
valyy151 <marin.valenta1@gmail.com>
2023-03-15 06:00:32 +0100

    Added quotes page

--- app.js
+++ app.js
@@ -4,33 +4,26 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (request, response, next) => {
-	console.log('here is homepage');
-	//  response.send('here is homepage');
-	response.sendFile(__dirname + '/views/home.html');
+app.get('/', (req, res, next) => {
+	res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get('/philosophy', (request, response, next) => {
-	//  response.send('here is homepage');
-	response.sendFile(__dirname + '/views/philosophy.html');
+app.get('/philosophy', (req, res, next) => {
+	res.sendFile(__dirname + '/views/philosophy.html');
 });
 
-app.get('/teachings', (request, response, next) => {
-	console.log('here is homepage');
-	//  response.send('here is homepage');
-	response.sendFile(__dirname + '/views/teachings.html');
+app.get('/teachings', (req, res, next) => {
+	res.sendFile(__dirname + '/views/teachings.html');
 });
 
-app.get('/quotes', (request, response, next) => {
-	//  response.send('here is homepage');
-	response.sendFile(__dirname + '/views/quotes.html');
+app.get('/quotes', (req, res, next) => {
+	res.sendFile(__dirname + '/views/quotes.html');
 });
 
-app.get('/legacy', (request, response, next) => {
-	//  response.send('here is homepage');
-	response.sendFile(__dirname + '/views/legacy.html');
+app.get('/legacy', (req, res, next) => {
+	res.sendFile(__dirname + '/views/legacy.html');
 });
 
 app.listen(3000, () => {
-	console.log('server start listening!!');
+	console.log('Server listening on port 3000');
 });
